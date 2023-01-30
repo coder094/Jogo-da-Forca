541779d5d38ed0779ce447722c5f97ea1771770e
kanekashi <87919814+kanekashi@users.noreply.github.com>
2023-01-30 19:17:09 -0300

    Add files via upload

---
+++ Jogo da forca/forca.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+int main(){
+
+    char palavra[30], letra[1],secreta[30];
+    int tam,i,chances,acertos;
+    bool acerto;
+
+    chances=6;
+    tam=0;
+    i=0;
+    acerto=false;
+    acertos=0;
+
+    cout << "Digite uma palavra: ";
+    cin >> palavra;
+    system("cls");
+
+
+    while(palavra[i] != '\0'){
+        i++;
+        tam++;
+    }
+
+    for (i=0;i<30; i++)
+    {
+        secreta[i] ='-';
+    }
+
+    while((chances > 0) && (acertos < tam)){
+
+        cout << "chances restantes: " << chances << "\n\n";
+        cout << "palavra secreta: " ;
+        for(i=0; i <tam; i++){
+            cout << secreta[i];
+        }
+        cout << "\n\nDiigite uma letra: ";
+        cin >> letra[0];
+        
+        for(i=0; i<tam; i++){
+            if(palavra[i]==letra[0]){
+                acerto = true;
+                secreta[i] = palavra[i];
+                acertos++;
+                }
+        }
+        if(!acerto){
+            chances--;
+        }
+        acerto = false;
+        system("cls");
+
+    }
+    if(acertos == tam){
+        cout << "Voce venceu!\n\n";
+        system("pause");
+    }else{
+        cout << "Que pena, voce perdeu!\n\n";
+        system("pause");
+    }
+    return 0;
+}
